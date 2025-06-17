85d977445ac9ed4db8806102647878d2792fc18b
Faye <seraphicfae@users.noreply.github.com>
2025-06-17 13:44:54 -0400

    Code refresh (big update soon)

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Lumina';
-export const SITE_DESCRIPTION = 'Welcome to my website! :3';
+export const SITE_TITLE = "Lumina";
+export const SITE_DESCRIPTION = "Welcome to my website! :3";
